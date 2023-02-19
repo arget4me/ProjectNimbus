51b8cc6afbfb90c8d5dce9c8c94bf22a12b043e8
Fria <fria.khorshid@gmail.com>
2023-02-19 20:39:13 +0100

    monsterDex now loading textures dynamically.
    MonsterDex loading textures to spirtes based on the monster name and displaying it.

--- CreatureCollector/Script/MonsterWiki.gd
+++ CreatureCollector/Script/MonsterWiki.gd
@@ -6,6 +6,7 @@ onready var monsterHealth = $MarginContainer/Panel/Health
 onready var monsterAttack = $MarginContainer/Panel/Attack
 onready var monsterDefence = $MarginContainer/Panel/Defence
 onready var monsterSpeed = $MarginContainer/Panel/Speed
+onready var monsterImage = $MarginContainer/Panel/MonsterImage
 var monsterList
 var monsterCounter
 
@@ -14,6 +15,19 @@ func _ready():
 	monsterList = MonsterDataSingelton.ParseAllMonstersJson("monsterdex")
 	setMonster(monsterList[monsterCounter])
 
+func findMonsterImage(var monsterName) -> String:
+	var imageDirectory = Directory.new()
+	if imageDirectory.open("res://Images") == OK:
+		imageDirectory.list_dir_begin()
+		var file_name = imageDirectory.get_next()
+		while file_name != "":
+			if file_name == (monsterName.to_lower() + ".png"):
+				return "res://Images/" + monsterName.to_lower() + ".png"
+			else:
+				file_name = imageDirectory.get_next()
+	
+	return "xd"
+
 func setMonster(var monster):
 	monsterName.text = monster.name
 	monsterDescription.text = monster.description
@@ -21,6 +35,7 @@ func setMonster(var monster):
 	monsterAttack.text = "Attack:" + str(monster.attack)
 	monsterDefence.text = "Defence:" + str(monster.defence)
 	monsterSpeed.text = "Speed:" + str(monster.speed)
+	monsterImage.texture = load(findMonsterImage(monster.name))
 
 func _on_Previous_pressed():
 	monsterCounter -= 1
