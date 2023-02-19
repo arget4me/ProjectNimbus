4d85aba32e1fae9e0c3ff0d9ed1f78060bfadc68
Fria <fria.khorshid@gmail.com>
2023-02-19 17:02:17 +0100

    MonsterDex UI
    start on monster dex ui element

---
+++ CreatureCollector/Script/MonsterWiki.gd
@@ -0,0 +1,28 @@
+extends CanvasLayer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+onready var nameVar = $MarginContainer/Panel/Name
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func getMonsters():
+	pass
+
+
+func _on_Previous_pressed():
+	nameVar.text = "APOSIjfaioHJF"
+
+
+func _on_Next_pressed():
+	pass # Replace with function body.
