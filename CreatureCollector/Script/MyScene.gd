d38afb6dee0b26a4d08b00459c2c9ad09cd5bf13
arget4me <sn.jn@telia.com>
2023-02-19 15:20:40 +0100

    Initial temp commit

---
+++ CreatureCollector/Script/MyScene.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	
+	if Input.is_action_pressed("ui_right"):
+		print("Hello World", delta)
+	if Input.is_action_pressed("bamse"):
+		print("Hello World Bamse", delta)
+	
+	
+	
+	
+	
+	pass
+
+func _physics_process(delta):
+	
+	
+	
+	
+	
+	pass
